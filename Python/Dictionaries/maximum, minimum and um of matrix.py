93d8e3686a299ec6ea07a13ea3330a2ae7cd187c
Ashwin Harish.P <99186533+AshwinHarishP@users.noreply.github.com>
2023-12-12 22:54:04 +0530

    Add files via upload

---
+++ Python/Dictionaries/maximum, minimum and um of matrix.py
@@ -0,0 +1,23 @@
+def convert_string_to_int(list_a):
+    new_list = []
+    for item in list_a:
+        num = int(item)
+        new_list.append(num)
+    return new_list
+
+
+m, n = input().split()
+m, n = int(m), int(n)
+num_list = []
+
+for i in range(m):
+    list_a = input().split()
+    list_a = convert_string_to_int(list_a)
+    num_list.extend(list_a)
+
+maximum = max(num_list)
+minimum = min(num_list)
+Sum =sum(num_list)
+print(maximum)
+print(minimum)
+print(Sum)
