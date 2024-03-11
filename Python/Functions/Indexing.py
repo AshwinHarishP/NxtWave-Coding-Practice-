5e7e4b5b30c59e1eec9e8bd03b2b006fc6c89ed4
Ashwin Harish.P <99186533+AshwinHarishP@users.noreply.github.com>
2024-03-11 22:16:19 +0400

    Add files via upload

---
+++ Python/Functions/Indexing.py
@@ -0,0 +1,8 @@
+def indexing(arg_1, arg_2):
+    indexed_string=word[index]
+    return indexed_string
+
+word = input()
+index = int(input())
+indexed_string=indexing(word,index)
+print(indexed_string)
